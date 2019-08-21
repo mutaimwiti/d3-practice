b3667ee9e504e633205c48dff97a924944ffc461
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-08-21 18:20:37 +0300

    Add scale and center chart

--- barChart/index.js
+++ barChart/index.js
@@ -1,22 +1,26 @@
 const data = [2, 8, 20, 47, 90];
 
-const svgWidth = 400;
-const svgHeight = 400;
+const svgWidth = 600;
+const svgHeight = 500;
 
-const barWidth = 50, barPadding = 10;
+const barWidth = 100, barPadding = 10;
 
 const canvas = d3.select('#chartArea')
   .append('svg')
   .attr('width', svgWidth)
   .attr('height', svgHeight);
 
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([0, svgHeight]);
+
 canvas.selectAll('rect')
   .data(data)
   .enter()
   .append('rect')
   .attr('width', barWidth - barPadding)
-  .attr('height', (d) => d)
-  .attr('y', (d) => svgHeight - d)
+  .attr('height', (d) => yScale(d))
+  .attr('y', (d) => svgHeight - yScale(d))
   .attr('transform', (d, i) => {
     const translation = [barWidth * i - barPadding, 0];
     return "translate(" + translation + ")"
