863d06adeac34dac65f359a62445dd42a9019329
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-08-21 18:41:26 +0300

    Add text labels on chart

--- barChart/index.js
+++ barChart/index.js
@@ -1,7 +1,7 @@
-const data = [2, 8, 20, 47, 90];
+const data = [2, 8, 20, 47, 80];
 
 const svgWidth = 600;
-const svgHeight = 500;
+const svgHeight = 800;
 
 const barWidth = 100, barPadding = 10;
 
@@ -12,7 +12,7 @@ const canvas = d3.select('#chartArea')
 
 const yScale = d3.scaleLinear()
   .domain([0, d3.max(data)])
-  .range([0, svgHeight]);
+  .range([0, svgHeight - 50]);
 
 canvas.selectAll('rect')
   .data(data)
@@ -25,3 +25,12 @@ canvas.selectAll('rect')
     const translation = [barWidth * i - barPadding, 0];
     return "translate(" + translation + ")"
   });
+
+canvas.selectAll('text')
+  .data(data)
+  .enter()
+  .append('text')
+  .text((d) => d)
+  .attr('y', (d) => svgHeight - yScale(d) + 15)
+  .attr('x', (d, i) => barWidth * i + barWidth / 4)
+  .attr("fill", "#f8f7e9");
